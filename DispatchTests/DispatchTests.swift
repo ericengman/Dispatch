b58f91fa8b338cd6b864494c5df2560e43d27da4
Eric Engman <eric.engman530@gmail.com>
2026-01-08 00:00:42 -0600

    Initial Commit

---
+++ DispatchTests/DispatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  DispatchTests.swift
+//  DispatchTests
+//
+//  Created by Eric Engman on 1/8/26.
+//
+
+import Testing
+@testable import Dispatch
+
+struct DispatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
